fb87f65b6132a2938824ce18d0f3b572f70e2445
Olibbbb <jakecanagasuriam@gmail.com>
2026-04-17 12:22:06 +0100

    initial day class

--- Weather.py
+++ Weather.py
@@ -1,1 +1,13 @@
-class Day
+class Day:
+    def __init__():
+        Day.currentDay = 0
+        Day.currentWeather = None
+        Day.weatherProbabilities = { #weather name and associated probability of blizzard
+            "promising" : 0.25,
+            "clear" : 0.2,
+            "hopeful" : 0.3,
+            "mixed" : 0.5,
+            "dreary" : 0.5,
+            "treacherous" : 0.6,
+            "okay" : 0.25
+        }
