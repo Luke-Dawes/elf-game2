a9590c3cd3b6539a1e8fa1ae162823caecccbd0d
Olibbbb <jakecanagasuriam@gmail.com>
2026-04-17 12:35:36 +0100

    day functions and weathers

--- Weather.py
+++ Weather.py
@@ -1,8 +1,11 @@
+import random
+
 class Day:
     def __init__():
         Day.currentDay = 0
+        Day.minimumDays = {} #min days for each location
         Day.currentWeather = None
-        Day.weathers = [
+        Day.weathers = [ #weather, probability of blizzard and prompt to be displayed
             {name: "promising", probability: 0.2, prompt: "The weather is looking promising!"},
             {name: "okay", probability: 0.25, prompt: "The weather is looking okay."},
             {name: "hopeful", probability: 0.15, prompt: "The weather is looking hopeful."},
@@ -11,3 +14,14 @@ class Day:
             {name: "uncertain", probability: 0.5, prompt: "The weather is looking uncertain..."},
             {name: "treacherous", probability: 0.75, prompt: "The weather is looking treacherous..."}
         ]
+
+    def selectNewWeather():
+        Day.currentWeather = random.choice(Day.weathers)
+
+    def enterNextDay():
+        Day.currentDay += 1
+        Day.selectNewWeather()
+
+
+
+    
